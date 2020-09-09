97ad880018399d2dd9fe41b3a5b4ca01fe052922
renegade <3102076112@qq.com>
2020-09-09 23:11:55 +0800

    detailed news增加关键词，详细新闻页面的card view变化为圆角。

--- app/src/main/java/com/taotianhua/covidnews/ui/home/detail/NewsDetailActivity.java
+++ app/src/main/java/com/taotianhua/covidnews/ui/home/detail/NewsDetailActivity.java
@@ -3,11 +3,14 @@ package com.taotianhua.covidnews.ui.home.detail;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.taotianhua.covidnews.repository.HistoryManager;
 import com.taotianhua.covidnews.R;
+
+import java.util.ArrayList;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class NewsDetailActivity extends AppCompatActivity {
     private TextView viewTitle;
     private TextView viewInfo;
     private TextView viewContent;
+    private TextView viewLabel;
 //    private LinearLayout layout;
 
 
@@ -31,6 +35,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         viewTitle = (TextView) findViewById(R.id.detail_title);
         viewContent = (TextView) findViewById(R.id.detail_content);
         viewInfo = (TextView) findViewById(R.id.detail_info);
+        viewLabel = (TextView) findViewById(R.id.detail_label);
         getIncomingIntent();
 
 
@@ -48,6 +53,17 @@ public class NewsDetailActivity extends AppCompatActivity {
             String timeLine =  "时间: " +((event.getTime() == null || event.getTime().isEmpty())? "未知" : event.getTime());
 
             viewInfo.setText(sourceLine + "\n" + timeLine);
+
+            ArrayList<String> labels = event.getLabels();
+            String labelStr = "#";
+            for(int i = 0; i < labels.size(); ++i){
+                labelStr += labels.get(i) + " #";
+            }
+            labelStr = labelStr.substring(0, labelStr.length()-1); //去掉最后一个#
+            viewLabel.setText(labelStr);
+//            viewLabel.setTextColor(Color.argb(1,40,155,226));
+
+
             HistoryManager.getInstance().addHistory(event.getId());
         });
 
